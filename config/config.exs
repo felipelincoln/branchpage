d30581b762d8f333d5d4964e5d37694fcf760562
Felipe LIncoln <felipelincoln@usp.br>
2021-01-09 11:55:19 -0300

    add production PORT config

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,7 @@ config :phoenix, :json_library, Jason
 
 config :web, Web.Endpoint,
   url: [host: "localhost"],
-  http: [port: 4000],
   secret_key_base: "zliHduy02MJH71NcPjCEcVsr7cv/EEna3wSILC4XEU2mAya0tPOsdABKUx2Z5ph2",
   render_errors: [view: Web.ErrorView, accepts: ~w(html), layout: false]
+
+import_config "#{Mix.env()}.exs"
---
+++ config/prod.exs
@@ -0,0 +1,4 @@
+import Config
+
+config :web, Web.Endpoint,
+  http: [port: {:system, "PORT"}]
