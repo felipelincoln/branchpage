c324fa6207f54bc3037cd3098cf3f9afc272a351
Felipe LIncoln <felipelincoln@usp.br>
2021-01-31 11:34:39 -0300

    add origin to endpoint

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,6 @@ import Config
 config :phoenix, :json_library, Jason
 
 config :web, Web.Endpoint,
-  url: [host: "localhost"],
   secret_key_base: "zliHduy02MJH71NcPjCEcVsr7cv/EEna3wSILC4XEU2mAya0tPOsdABKUx2Z5ph2",
   render_errors: [view: Web.ErrorView, accepts: ~w(html), layout: {Web.LayoutView, :base}],
   pubsub_server: Web.PubSub,
