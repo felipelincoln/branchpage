9c2e602e17651c83aa8b39acab31a91ec72e6771
Felipe LIncoln <felipelincoln@usp.br>
2021-01-31 17:44:16 -0300

    add ssl configuration

--- config/prod.exs
+++ config/prod.exs
@@ -3,4 +3,5 @@ import Config
 config :web, Web.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("HOST")],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
