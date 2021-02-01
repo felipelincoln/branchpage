048e715371679c99876428fd3512d05db2a81eef
Felipe LIncoln <felipelincoln@usp.br>
2021-01-31 23:42:39 -0300

    set host to nil

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,6 @@ import Config
 
 config :web, Web.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("HOST")],
-  force_ssl: [host: nil, rewrite_on: [:x_forwarded_proto]],
+  url: [host: nil],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
