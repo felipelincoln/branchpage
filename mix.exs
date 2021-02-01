4811eef9f26b2671f19c4f588317dd89b86756e4
Felipe LIncoln <felipelincoln@usp.br>
2021-02-01 01:02:27 -0300

    fix mix test env

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule BranchPage.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: System.get_env("MIX_ENV") == :prod,
+      preferred_cli_env: [test: :test],
       deps: deps()
     ]
   end
