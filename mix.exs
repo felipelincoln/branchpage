9b3348df5f10defeb01846c1f2a9995643c6e1dc
Felipe LIncoln <felipelincoln@usp.br>
2021-02-07 10:11:27 -0300

    add MIX_ENV to mix test

--- mix.exs
+++ mix.exs
@@ -5,8 +5,7 @@ defmodule BranchPage.MixProject do
     [
       apps_path: "apps",
       version: "0.1.0",
-      start_permanent: System.get_env("MIX_ENV") == :prod,
-      preferred_cli_env: [test: :test],
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
