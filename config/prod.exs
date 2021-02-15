861dcd079e00231c09d88fb396c4d5853b7f46af
Felipe LIncoln <felipelincoln@usp.br>
2021-02-15 16:51:02 -0300

    revert force_ssl and add host nil

--- config/prod.exs
+++ config/prod.exs
@@ -3,4 +3,5 @@ import Config
 config :web, Web.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: nil],
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
   cache_static_manifest: "priv/static/cache_manifest.json"
