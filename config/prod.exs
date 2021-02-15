52b431e79c2f4db78cc19d7af3dc703e18de4f0a
Felipe LIncoln <felipelincoln@usp.br>
2021-02-15 17:44:05 -0300

    add HOST env to endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,6 @@ import Config
 
 config :web, Web.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: nil],
+  url: [scheme: "https", host: System.get_env("HOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
   cache_static_manifest: "priv/static/cache_manifest.json"
