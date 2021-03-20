3afcb22431ee3a8831b3a7e14057445d443e51e9
Felipe Lincoln <felipelincoln@usp.br>
2021-03-20 17:27:49 -0300

    remove url: nil

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,6 @@ import Config
 
 config :web, Web.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: nil,
   force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
