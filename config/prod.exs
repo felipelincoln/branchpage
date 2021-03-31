c6cffa1e2ba46f95a46a376c94d359d289c5b22d
Felipe LIncoln <felipelincoln@usp.br>
2021-03-31 01:34:53 -0300

    add subdomain to check origin

--- config/prod.exs
+++ config/prod.exs
@@ -3,6 +3,6 @@ import Config
 config :web, Web.Endpoint,
   http: [port: {:system, "PORT"}],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  check_origin: ["//branchpage.com"]
+  check_origin: ["//branchpage.com", "//*.branchpage.com"]
 
 config :logger, level: :info
