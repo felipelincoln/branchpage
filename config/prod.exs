887097d70929a2533f111d4a7c0c4d8de0214554
Felipe LIncoln <felipelincoln@usp.br>
2021-03-31 01:27:56 -0300

    add check origin

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,7 @@ import Config
 
 config :web, Web.Endpoint,
   http: [port: {:system, "PORT"}],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["//branchpage.com"]
 
 config :logger, level: :info
