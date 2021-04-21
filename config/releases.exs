b7a9cbd4253070aef9af0e50f1a251ad53357c84
Felipe LIncoln <felipelincoln@usp.br>
2021-04-21 01:52:05 -0300

    fix raises

--- config/releases.exs
+++ config/releases.exs
@@ -1,12 +1,10 @@
 import Config
 
-config :web, Web.Endpoint, secret_key_base: get_or_raise("SECRET_KEY_BASE")
+config :web, Web.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE") || raise("SECRET_KEY_BASE is missing.")
 
 config :blog, Blog.Repo,
   start_apps_before_migration: [:ssl],
   ssl: true,
-  url: get_or_raise("DATABASE_URL"),
+  url: System.get_env("DATABASE_URL") || raise("DATABASE_URL is missing."),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
-defp get_or_raise(env_var),
-  do: System.get_env(env_var) || raise("Missing environment variable #{env_var}")
