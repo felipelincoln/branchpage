fe3ac148fba26fc45ac6befae09dde7b030e4432
Felipe LIncoln <felipelincoln@usp.br>
2021-05-09 19:15:57 -0300

    add integration tests

--- apps/publishing/lib/publishing/integration.ex
+++ apps/publishing/lib/publishing/integration.ex
@@ -5,6 +5,20 @@ defmodule Publishing.Integration do
 
   alias Publishing.Integration.Github
 
+  @doc """
+  Returns the `url`'s integration module.
+
+  ## Currently supported integrations
+  * Github: `Publishing.Integration.Github`
+
+  Examples:
+      iex> service("https://github.com/teste")
+      {:ok, Publishing.Integration.Github}
+
+      iex> service("https://gitlab.com/teste")
+      {:error, :integration}
+  """
+  @spec service(String.t()) :: {:ok, module} | {:error, :integration}
   def service(url) do
     case URI.parse(url) do
       %URI{host: "github.com"} -> {:ok, Github}
