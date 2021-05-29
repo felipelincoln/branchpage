ecac055f17f98fd0335212423692f979d052532d
Felipe LIncoln <felipelincoln@usp.br>
2021-05-29 17:35:50 -0300

    fix tests

--- apps/publishing/test/publishing/manage_test.exs
+++ apps/publishing/test/publishing/manage_test.exs
@@ -41,20 +41,20 @@ defmodule Publishing.ManageTest do
   end
 
   test "build_article/1 non existing url returns 404" do
-    expect(TeslaMock, :call, &api/2)
+    expect(TeslaMock, :call, &raw/2)
 
     assert {:error, "Page not found"} = Manage.build_article(@invalid_404_url)
   end
 
   test "build_article/1 on invalid url returns status code" do
-    expect(TeslaMock, :call, &api/2)
+    expect(TeslaMock, :call, &raw/2)
 
     assert {:error, "Failed to retrieve page content. (error 500)"} =
              Manage.build_article(@invalid_500_url)
   end
 
   test "build_article/1 on valid url returns article" do
-    expect(TeslaMock, :call, &api/2)
+    expect(TeslaMock, :call, &raw/2)
 
     assert {:ok, %Article{} = article} = Manage.build_article(@valid_url)
 
@@ -66,14 +66,14 @@ defmodule Publishing.ManageTest do
   end
 
   test "save_article/1 saves an article" do
-    expect(TeslaMock, :call, &api/2)
+    expect(TeslaMock, :call, &raw/2)
     {:ok, %Article{} = article} = Manage.build_article(@valid_url)
 
     assert {:ok, %Article{}} = Manage.save_article(article)
   end
 
   test "save_article/1 on existing article returns error" do
-    expect(TeslaMock, :call, &api/2)
+    expect(TeslaMock, :call, &raw/2)
     {:ok, %Article{} = article} = Manage.build_article(@valid_url)
 
     assert {:ok, %Article{}} = Manage.save_article(article)
@@ -81,7 +81,7 @@ defmodule Publishing.ManageTest do
   end
 
   test "load_article!/2 loads an article from database" do
-    expect(TeslaMock, :call, 2, &api/2)
+    expect(TeslaMock, :call, 2, &raw/2)
     {:ok, %Article{} = article} = Manage.build_article(@valid_url)
     {:ok, %Article{id: id}} = Manage.save_article(article)
 
@@ -94,8 +94,8 @@ defmodule Publishing.ManageTest do
   end
 
   test "load_article!/2 updates title and preview" do
-    expect(TeslaMock, :call, &api/2)
-    expect(TeslaMock, :call, &api_updated/2)
+    expect(TeslaMock, :call, &raw/2)
+    expect(TeslaMock, :call, &raw_updated/2)
 
     {:ok, %Article{} = article} = Manage.build_article(@valid_url)
     {:ok, %Article{id: id}} = Manage.save_article(article)
@@ -110,8 +110,8 @@ defmodule Publishing.ManageTest do
   end
 
   test "load_article!/2 deletes article if deleted from integration" do
-    expect(TeslaMock, :call, &api/2)
-    expect(TeslaMock, :call, &api_deleted/2)
+    expect(TeslaMock, :call, &raw/2)
+    expect(TeslaMock, :call, &raw_deleted/2)
 
     {:ok, %Article{} = article} = Manage.build_article(@valid_url)
 
@@ -127,7 +127,7 @@ defmodule Publishing.ManageTest do
   end
 
   test "load_article!/2 on non matching username raises PageNotFound" do
-    expect(TeslaMock, :call, 2, &api/2)
+    expect(TeslaMock, :call, 2, &raw/2)
     {:ok, %Article{} = article} = Manage.build_article(@valid_url)
     {:ok, %Article{}} = Manage.save_article(article)
 
@@ -138,23 +138,39 @@ defmodule Publishing.ManageTest do
     assert_raise Publishing.PageNotFound, fn -> Manage.load_blog!("") end
   end
 
-  test "load_blog!/1 return blogs with preloaded articles" do
-    blog = Factory.insert(:blog, username: "test")
+  test "load_blog!/1 return blogs with preloaded articles", %{platform: platform} do
+    expect(TeslaMock, :call, &api/2)
+
+    blog = Factory.insert(:blog, username: "test", platform_id: platform.id)
     _articles = Factory.insert_pair(:article, blog_id: blog.id)
 
     assert (%Blog{} = blog) = Manage.load_blog!("test")
     assert [_, _] = blog.articles
   end
 
-  defp api_deleted(%{url: @valid_raw_url}, _), do: {:ok, %{status: 404}}
-  defp api_updated(%{url: @valid_raw_url}, _), do: {:ok, %{status: 200, body: @updated_body}}
-  defp api(%{url: @valid_raw_url}, _), do: {:ok, %{status: 200, body: @valid_body}}
-  defp api(%{url: @invalid_404_raw_url}, _), do: {:ok, %{status: 404}}
-  defp api(%{url: @invalid_500_raw_url}, _), do: {:ok, %{status: 500}}
+  defp raw_deleted(%{url: @valid_raw_url}, _), do: {:ok, %{status: 404}}
+  defp raw_updated(%{url: @valid_raw_url}, _), do: {:ok, %{status: 200, body: @updated_body}}
+  defp raw(%{url: @valid_raw_url}, _), do: {:ok, %{status: 200, body: @valid_body}}
+  defp raw(%{url: @invalid_404_raw_url}, _), do: {:ok, %{status: 404}}
+  defp raw(%{url: @invalid_500_raw_url}, _), do: {:ok, %{status: 500}}
+
+  defp api(%{url: "https://api.github.com/graphql"}, _) do
+    response = %{
+      "data" => %{
+        "user" => %{
+          "name" => "as",
+          "bio" => "as",
+          "avatarUrl" => "adad"
+        }
+      }
+    }
+
+    {:ok, %{body: response, status: 200}}
+  end
 
   defp create_github_platform(_) do
-    Factory.insert(:platform, name: "https://github.com/")
+    platform = Factory.insert(:platform, name: "https://github.com/")
 
-    :ok
+    %{platform: platform}
   end
 end
