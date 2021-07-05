d0451475a29f25349bb3eed1ec562e231b17aa68
Felipe LIncoln <felipelincoln@usp.br>
2021-07-04 22:40:48 -0300

    create scroll webhook

--- apps/web/assets/js/main.js
+++ apps/web/assets/js/main.js
@@ -4,7 +4,16 @@ import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 import Prism from "prismjs"
 
+let scrollAt = () => {
+  let scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+  let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+  let clientHeight = document.documentElement.clientHeight
+
+  return scrollTop / (scrollHeight - clientHeight) * 100
+}
+
 let Hooks = {}
+
 Hooks.CodeHighlight = {
   mounted(){
     this.handleEvent("highlightAll", () => Prism.highlightAll());
@@ -14,6 +23,17 @@ Hooks.CodeHighlight = {
 Hooks.PublishButton = {
   mounted(){
     this.handleEvent("scrollTop", () => window.scrollTo(0, 0));
+  }
+}
+
+Hooks.InfiniteScroll = {
+  mounted(){
+    window.addEventListener("scroll", e => {
+      if(scrollAt() > 99){
+        console.log("chamando evento")
+        this.pushEvent("load-more", {})
+      }
+    })
   }
 }
 
