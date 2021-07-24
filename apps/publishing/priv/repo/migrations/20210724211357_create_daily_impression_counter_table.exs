7655b4dc0a8434b5f34d23d53e608a2b0e3e2f7c
Felipe LIncoln <felipelincoln@usp.br>
2021-07-24 18:28:00 -0300

    rename impression counter table

--- apps/publishing/priv/repo/migrations/20210724211357_create_daily_impression_counter_table.exs
+++ apps/publishing/priv/repo/migrations/20210724211357_create_daily_impression_counter_table.exs
@@ -1,8 +1,8 @@
-defmodule Publishing.Repo.Migrations.CreateDayImpressionsTable do
+defmodule Publishing.Repo.Migrations.CreateDailyImpressionCounterTable do
   use Ecto.Migration
 
   def change do
-    create table(:day_impressions) do
+    create table(:daily_impression_counter) do
       add :count, :integer
       add :day, :date, default: fragment("now()")
       add :article_id, references(:article, type: :uuid)
