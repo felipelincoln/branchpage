7aae290e5d294b74b87e6ae4f70b68cb10d765cb
Felipe LIncoln <felipelincoln@usp.br>
2021-07-31 19:46:14 -0300

    add graph js hook

--- apps/web/lib/web/live/dashboard_live.ex
+++ apps/web/lib/web/live/dashboard_live.ex
@@ -22,6 +22,8 @@ defmodule Web.DashboardLive do
 
       articles_impressions = Interact.user_impressions_by_date(user, Date.utc_today())
 
+      {graph_dates, graph_impressions} = build_graph_data(user)
+
       socket =
         socket
         |> assign(:meta, @meta)
@@ -30,6 +32,9 @@ defmodule Web.DashboardLive do
         |> assign(:articles_impressions, articles_impressions)
         |> assign(:graph_impressions, articles_impressions)
         |> assign(:graph_date, Date.utc_today())
+        |> assign(:graph_index, "27")
+        |> assign(:graph_dates, graph_dates)
+        |> assign(:graph_impressions, graph_impressions)
 
       {:ok, socket}
     else
@@ -41,8 +46,47 @@ defmodule Web.DashboardLive do
         |> assign(:articles_impressions, 0)
         |> assign(:graph_impressions, 0)
         |> assign(:graph_date, Date.utc_today())
+        |> assign(:graph_index, "27")
+        |> assign(:graph_dates, %{})
+        |> assign(:graph_impressions, %{})
 
       {:ok, socket}
     end
+  end
+
+  @impl true
+  def handle_event("get-graph-impressions", %{"barIndex" => graph_index}, socket) do
+    socket =
+      socket
+      |> assign(:graph_index, graph_index)
+
+    {:noreply, socket}
+  end
+
+  defp build_graph_data(user_id) do
+    data =
+      for days_ago <- 0..27 do
+        key = Integer.to_string(27 - days_ago)
+
+        date =
+          Date.utc_today()
+          |> Date.add(-days_ago)
+
+        impressions = Interact.user_impressions_by_date(user_id, date)
+
+        {key, date, impressions}
+      end
+
+    impressions_map =
+      data
+      |> Enum.map(fn {key, _, impr} -> {key, impr} end)
+      |> Map.new()
+
+    dates_map =
+      data
+      |> Enum.map(fn {key, date, _} -> {key, format_date(date)} end)
+      |> Map.new()
+
+    {dates_map, impressions_map}
   end
 end
