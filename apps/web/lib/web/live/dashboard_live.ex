94817437eb654418786f09f37cf7a21befbabaed
Felipe LIncoln <felipelincoln@usp.br>
2021-07-31 18:11:18 -0300

    check socket connection

--- apps/web/lib/web/live/dashboard_live.ex
+++ apps/web/lib/web/live/dashboard_live.ex
@@ -13,19 +13,30 @@ defmodule Web.DashboardLive do
 
   @impl true
   def mount(_args, %{"current_user" => user}, socket) do
-    articles = Manage.articles_by_blog(user)
-    articles_count = length(articles)
+    if connected?(socket) do
+      articles = Manage.articles_by_blog(user)
+      articles_count = length(articles)
 
-    articles_impressions =
-      Enum.reduce(articles, 0, fn %{impressions_total: total}, acc -> acc + total end)
+      articles_impressions =
+        Enum.reduce(articles, 0, fn %{impressions_total: total}, acc -> acc + total end)
 
-    socket =
-      socket
-      |> assign(:meta, @meta)
-      |> assign(:articles, articles)
-      |> assign(:articles_count, articles_count)
-      |> assign(:articles_impressions, articles_impressions)
+      socket =
+        socket
+        |> assign(:meta, @meta)
+        |> assign(:articles, articles)
+        |> assign(:articles_count, articles_count)
+        |> assign(:articles_impressions, articles_impressions)
 
-    {:ok, socket}
+      {:ok, socket}
+    else
+      socket =
+        socket
+        |> assign(:meta, @meta)
+        |> assign(:articles, [])
+        |> assign(:articles_count, 0)
+        |> assign(:articles_impressions, 0)
+
+      {:ok, socket}
+    end
   end
 end
