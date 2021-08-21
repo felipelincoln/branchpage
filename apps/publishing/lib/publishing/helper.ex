3b0aacc4b0c81f99eb94575b2533b8ebd629a2bf
Felipe LIncoln <felipelincoln@usp.br>
2021-08-21 09:28:37 -0300

    refactor format_date

--- apps/publishing/lib/publishing/helper.ex
+++ apps/publishing/lib/publishing/helper.ex
@@ -3,34 +3,31 @@ defmodule Publishing.Helper do
   Helpers for the publishing application.
   """
 
-  @current_year Date.utc_today().year
-  @last_year @current_year - 1
-
   @doc """
   Formats a datetime into "Month. day" format
-
-  Examples
-      iex> format_date(~D[2021-06-15])
-      "Jun 15"
-
-      iex> format_date(~D[2020-06-21])
-      "Last year"
-
-      iex> format_date(~D[2019-12-12])
-      "2 years ago"
   """
-  def format_date(%{year: @current_year} = datetime) do
-    Timex.format!(datetime, "%b %e", :strftime)
-  end
+  def format_date(today \\ Date.utc_today(), datetime) do
+    this_day = today.day
+    this_year = today.year
+    yesterday = Date.add(today, -1).day
+    past_year = today.year - 1
 
-  def format_date(%{year: @last_year}) do
-    "Last year"
-  end
+    case datetime do
+      %{day: ^this_day} ->
+        "Today"
 
-  def format_date(%{year: year}) do
-    n = @current_year - year
-    "#{n} years ago"
-  end
+      %{day: ^yesterday} ->
+        "Yesterday"
 
-  def format_date(_), do: ""
+      %{year: ^this_year} ->
+        Timex.format!(datetime, "%b %e", :strftime)
+
+      %{year: ^past_year} ->
+        "A year ago"
+
+      %{year: year} ->
+        years_ago = this_year - year
+        "#{years_ago} years ago"
+    end
+  end
 end
