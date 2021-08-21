7adf962445b888e3d8696ca25131b5b4b85ee125
Felipe LIncoln <felipelincoln@usp.br>
2021-08-21 09:49:18 -0300

    add interact tests

--- apps/publishing/test/publishing/interact_test.exs
+++ apps/publishing/test/publishing/interact_test.exs
@@ -43,4 +43,64 @@ defmodule Publishing.InteractTest do
 
     assert %{impressions_total: 635} = Interact.put_impressions(article)
   end
+
+  test "user_impressions_by_date/2 returns all user's impressions on a date" do
+    blog = Factory.insert(:blog)
+    article = Factory.insert(:article, blog_id: blog.id)
+    article_2 = Factory.insert(:article, blog_id: blog.id)
+
+    _imp_1 =
+      Factory.insert(:daily_impression_counter, %{
+        article_id: article.id,
+        count: 100,
+        day: ~D[2021-01-01]
+      })
+
+    _imp_2 =
+      Factory.insert(:daily_impression_counter, %{
+        article_id: article.id,
+        count: 505,
+        day: ~D[2021-01-02]
+      })
+
+    _imp_2 =
+      Factory.insert(:daily_impression_counter, %{
+        article_id: article_2.id,
+        count: 30,
+        day: ~D[2021-01-02]
+      })
+
+    assert Interact.user_impressions_by_date(blog.id, ~D[2021-01-01]) == 100
+    assert Interact.user_impressions_by_date(blog.id, ~D[2021-01-02]) == 535
+    assert Interact.user_impressions_by_date(blog.id, ~D[2021-06-01]) == 0
+  end
+
+  test "user_impressions_total/1 returns all user's impressions" do
+    blog = Factory.insert(:blog)
+    article = Factory.insert(:article, blog_id: blog.id)
+    article_2 = Factory.insert(:article, blog_id: blog.id)
+
+    _imp_1 =
+      Factory.insert(:daily_impression_counter, %{
+        article_id: article.id,
+        count: 100,
+        day: ~D[2021-01-01]
+      })
+
+    _imp_2 =
+      Factory.insert(:daily_impression_counter, %{
+        article_id: article.id,
+        count: 505,
+        day: ~D[2021-01-02]
+      })
+
+    _imp_2 =
+      Factory.insert(:daily_impression_counter, %{
+        article_id: article_2.id,
+        count: 30,
+        day: ~D[2021-01-02]
+      })
+
+    assert Interact.user_impressions_total(blog.id) == 635
+  end
 end
